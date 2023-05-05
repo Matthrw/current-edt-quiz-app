1a5ed9831aa8a178b906b1e96fe9383105baf134
matthrw <opeyemi.s.olushola@gmail.com>
2023-05-05 04:00:07 -0700

    new frontend and backend commit

---
+++ client/src/components/App.js
@@ -0,0 +1,37 @@
+import '../styles/App.css';
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import component
+import Main from './useMain';
+import Quiz from './Quiz';
+import Result from './Result';
+// import CreateQuestionPage from './CreateQuestionPage';
+import { CheckUserExist } from '../helper/helper';
+/**react routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Main></Main>
+  },
+   {
+    path: '/quiz',
+    element: <CheckUserExist> <Quiz></Quiz></CheckUserExist>
+ },
+  {
+    path: '/result',
+    element:<CheckUserExist><Result></Result></CheckUserExist>
+  },
+
+])
+function App() {
+  return (
+    <>
+      <RouterProvider router={router
+      }></RouterProvider>
+      
+    </>
+  );
+}
+
+export default App;
